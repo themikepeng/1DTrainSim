23c242aa43c5788792af2f406c0d1ecf6b99f32c
themikepeng <mipeng@berkeley.edu>
2023-06-18 23:59:00 -0700

    V0.2.0 (2023-06-18)

--- CTrain.py
+++ CTrain.py
@@ -9,7 +9,17 @@ MPH_TO_M_S = 0.44704
 MI_TO_M = 1609.344
 IN_TO_M = 0.0254
 
-### print() helpers to show dual units ###
+### braking performance params ###
+### use default constants for now ###
+'''
+BRAKE_A1 = deceleration rate (m/s^2) from BRAKE_V1 (m/s) down to 0
+BRAKE_A2 = deceleration rate for v above BRAKE_V1
+'''
+BRAKE_A1 = 1.35 * MPH_TO_M_S
+BRAKE_A2 = 2 * MPH_TO_M_S
+BRAKE_V1 = 125 * MPH_TO_M_S
+
+### helpers to print dual units, given metric ###
 def mass_units_str(m):
   return str(round(m, 2)) + " kg (" + str(round(m/LB_TO_KG, 2)) + " lb)"
 
@@ -32,28 +42,45 @@ def t_round_str(t):
 def calc_D(h_in, w_in, rho = 1.2041, C_d = 1):
   '''
   args:
-  h = max cross-section height
-  w = max cross-section width
+  h = max cross-section height (in)
+  w = max cross-section width (in)
   C_d = coefficient of drag
 
   returns:
-  D = simplified coefficient of drag
+  D = combined coefficient of drag
   '''
+  assert all([h_in > 0, w_in > 0, rho > 0, C_d > 0]), \
+    "The following args must be positive: all"
   h = h_in * IN_TO_M
   w = w_in * IN_TO_M
+
+  # implements equation 1.3.1
   D_final = 0.5 * h * w * rho * C_d
   return D_final
 
+def calc_avg_vel(d, t):
+  '''Returns v_avg given any distance and time'''
+  assert all([d > 0, t > 0]), \
+    "The following args must be positive: all"
+  
+  # implements equation 3.2.2
+  return d / t
+
 
 class Train():
-  def __init__(self, m_lb, P_hp, F_lbf, D=0):
+  def __init__(self, m_lb, P_hp, F_lbf, D=0.77):
     '''
     Initialize a Train from args:
     m_lb = mass of train (lb)
     P = traction power (hp)
     F_lbf = max tractive effort (lbf)
-    D = simplified coefficient of drag
+    D = combined coefficient of drag (optional)
     '''
+    assert all([m_lb > 0, P_hp > 0, F_lbf > 0]), \
+      "The following args must be positive: m_lb, P_hp, F_lbf"
+    assert D >= 0, \
+      "The following args must be nonnegative: D"
+    
     self.m = m_lb * LB_TO_KG
     self.P = P_hp * HP_TO_W
     self.F = F_lbf * LBF_TO_N
@@ -66,20 +93,23 @@ class Train():
     Calculate v_1, the highest v where full F can be applied
     and t_1, the time to accelerate to v_1
     '''
-    # implement formula <FILL IN>
+    # implements equation 1.1.1
     self.v_1 = self.P/self.F
-    # implement formula <FILL IN>
+    # implements equation 1.1.2
     self.t_1 = self.m * self.P / (self.F**2)
-    print(f"Power limits traction above {vel_units_str(self.v_1)}, after {t_round_str(self.t_1)}")
+    print(f"Traction limited by power above {vel_units_str(self.v_1)}, after {t_round_str(self.t_1)}")
 
-  def calc_time_vel(self, v_mph):
-    '''Returns the time required to reach v_mph (-1 if never)'''
+  def calc_accel_time(self, v_mph):
+    '''Returns the time required to reach v_mph, -1 if unable'''
+    assert v_mph >= 0, \
+      "The following args must be nonnegative: v_mph"
     v = v_mph * MPH_TO_M_S
+
     if v <= self.v_1:
-      # implement formula <FILL IN>
+      # implements equation 1.2.2
       t_final = self.m * v / self.F
     else:
-      # implement formula <FILL IN>
+      # implements equation 1.3.2
       numer = self.m * (v**2 - self.v_1)
       denom = 2 * (self.P - (self.D * v**3))
       if denom < 0:
@@ -87,16 +117,19 @@ class Train():
       t_final = (numer / denom) + self.t_1
     return t_final
 
-  def calc_vel_time(self, t):
+  def calc_accel_vel(self, t):
     '''
     Returns the velocity after t s of acceleration
     by solving for v in equation of form av^3 + bv^2 + c = 0
     '''
+    assert t >= 0, \
+      "The following args must be nonnegative: t"
+
     if t < self.t_1:
-      # implement formula <FILL IN>
+      # implements equation 1.2.1
       vel_final = self.F * t / self.m
     else:
-      # implement formula <FILL IN>
+      # implements equation 1.3.3
       a = 2 * self.D * (t - self.t_1)
       b = self.m
       c = (2 * self.P * (self.t_1 - t)) - (self.m * self.v_1)
@@ -106,12 +139,92 @@ class Train():
       vel_final = abs(roots[-1])
     return vel_final
 
-  def calc_dist_time(self, t):
+  def calc_accel_dist(self, t):
     '''
-    Returns the dist traveled (displacement) after t s of acceleration
+    Returns the dist traveled after t s of acceleration
     by integrating v(t)dt over [0, t]
     '''
-    res, err = quad(self.calc_vel_time, 0, t)
+    assert t > 0, \
+      "The following args must be positive: t"
+
+    # implements equation 1.3.4
+    res, err = quad(self.calc_accel_vel, 0, t)
     # ensure integral calculation is accurate
     assert err < res * 0.01, f"Distance calculated with high error +-{err}%"
     return res
+
+  def calc_brake_time(self, v_mph):
+    '''Returns the time required to brake to a stop from v_mph'''
+    assert v_mph >= 0, \
+      "The following args must be nonnegative: v_mph"
+    
+    # implements equation 2.1.1
+    v = v_mph * MPH_TO_M_S
+    if v < BRAKE_V1:
+      t_final = v * (1 / BRAKE_A1)
+    else:
+      t_final = (v - BRAKE_V1) * (1 / BRAKE_A2) + BRAKE_V1 * (1 / BRAKE_A1)
+    return t_final
+
+  def calc_brake_dist(self, v_mph):
+    '''Returns the dist required to brake to a stop from v_mph'''
+    assert v_mph >= 0, \
+      "The following args must be nonnegative: v_mph"
+    v = v_mph * MPH_TO_M_S
+
+    # implements equation 2.1.2
+    if v < BRAKE_V1:
+      d_final = 0.5 * (v ** 2) / BRAKE_A1
+    else:
+      add1 = BRAKE_V1 * (v - BRAKE_V1) * (1 / BRAKE_A2)
+      add2 = 0.5 * ((v - BRAKE_V1) ** 2) / BRAKE_A2
+      add3 = 0.5 * (BRAKE_V1 ** 2) / BRAKE_A1
+      d_final = add1 + add2 + add3
+    return d_final
+
+  def stop_to_stop_time(self, d_tot_mi, v_max_mph, t_dwell=60):
+    '''
+    Returns the total arrival-to-arrival travel time from one stop to the next
+    d_mi = track distance between the two stops
+    vmax_mph = practical top track speed
+    t_dwell (optional) = dwell time at first stop
+    '''
+    assert all([d_tot_mi > 0, v_max_mph > 0]), \
+      "The following args must be positive: d_tot_mi, v_max_mph"
+    assert t_dwell >= 0, \
+      "The following args must be nonnegative: t_dwell"
+    
+    v_max = v_max_mph * MPH_TO_M_S
+    d_tot = d_tot_mi * MI_TO_M
+
+    t_acc = self.calc_accel_time(v_max_mph)
+    d_acc = self.calc_accel_dist(t_acc)
+    t_brake = self.calc_brake_time(v_max_mph)
+    d_brake = self.calc_brake_dist(v_max_mph)
+
+    #implements algorithm 3.3.2
+    while not (d_acc + d_brake <= d_tot):
+      print(f"{dist_units_str(d_tot)} is insufficient to accelerate to and brake from {vel_units_str(v_max)}! Lowering v_max by 0.5%")
+      v_max_mph =  v_max_mph * 0.995
+      v_max = v_max_mph * MPH_TO_M_S
+
+      t_acc = self.calc_accel_time(v_max_mph)
+      d_acc = self.calc_accel_dist(t_acc)
+      t_brake = self.calc_brake_time(v_max_mph)
+      d_brake = self.calc_brake_dist(v_max_mph)
+
+    print(f"Returning stop-to-stop travel time: distance {dist_units_str(d_tot)}, practical top speed {vel_units_str(v_max)}, dwell time {t_dwell} s")
+    #implements equation 3.1.1
+    d_vmax = d_tot - d_acc - d_brake
+    #implements equation 3.1.2
+    t_vmax = d_vmax / v_max
+
+    #implements equation 3.2.1
+    t_total = t_dwell + t_acc + t_vmax + t_brake
+    
+    return t_total
+
+  def show_accel_curve(self):
+    '''Displays a graph of v as a function of t'''
+    #TODO
+    pass
--- tester.py
+++ tester.py
@@ -1,14 +1,70 @@
 from CTrain import *
 
 if __name__ == "__main__":
+    # demonstration pt. 1
+    # instantiation, acceleration, deceleration, stop-to-stop calculations
+    # expect no errors
     D = calc_D(h_in=15*12+7.5, w_in=10*12+3, C_d=0.77)
-    print(D)
+    print(f"combined coefficient of drag: {D}")
     train = Train(m_lb=1030000, P_hp=3900, F_lbf=50000, D=D)
+
+    print()
     test_vel = 125
-    print(f"time (s) to reach {test_vel} mph: ")
-    test_time = train.calc_time_vel(test_vel)
-    print(test_time)
-    print(f"speed at time {test_time}: ")
-    print(vel_units_str(train.calc_vel_time(test_time)))
-    print(f"distance traveled at time {test_time}: ")
-    print(dist_units_str(train.calc_dist_time(test_time)))
+    print(f"time to reach {test_vel} mph: ")
+    test_time = train.calc_accel_time(test_vel)
+    print(f"{test_time} s")
+
+    print(f"speed at time {test_time} s: ")
+    print(vel_units_str(train.calc_accel_vel(test_time)))
+
+    print(f"distance traveled accelerating for {test_time} s: ")
+    print(dist_units_str(train.calc_accel_dist(test_time)))
+
+    print()
+    print(f"time to brake from {test_vel} mph: ")
+    print(f"{train.calc_brake_time(test_vel)} s")
+
+    print(f"distance traveled while braking from {test_vel} mph: ")
+    print(dist_units_str(train.calc_brake_dist(test_vel)))
+
+    print()
+    test_total_dist = 15
+    test_total_dist_m = test_total_dist * MI_TO_M
+    test_total_time = train.stop_to_stop_time(d_tot_mi=test_total_dist, v_max_mph=test_vel)
+    print(f"{test_total_time} s")
+    print(f"average speed: {vel_units_str(calc_avg_vel(test_total_dist_m, test_total_time))} mph")
+    
+    
+    # demonstration pt. 2
+    # special case: distance-constrained stop-to-stop calculation
+    # expect no errors
+    test_total_dist = 5
+    test_total_dist_m = test_total_dist * MI_TO_M
+    test_total_time = train.stop_to_stop_time(d_tot_mi=test_total_dist, v_max_mph=test_vel)
+    print(f"{test_total_time} s")
+    print(f"average speed: {vel_units_str(calc_avg_vel(test_total_dist_m, test_total_time))} mph")
+    
+    # edge test cases
+    # expect no errors
+    weak_train = Train(m_lb=0.1, P_hp=0.1, F_lbf=0.1, D=0)
+    train.calc_accel_time(0)
+    train.calc_accel_vel(0)
+    train.calc_accel_dist(0.1)
+    train.calc_brake_time(0)
+    train.calc_brake_dist(0)
+    train.stop_to_stop_time(d_tot_mi=0.1, v_max_mph=0.1, t_dwell=0)
+    
+    # bad test cases
+    # expect error if any line(s) activated
+    #bad_train = Train(m_lb=0, P_hp=0.1, F_lbf=0.1, D=0)
+    #bad_train2 = Train(m_lb=0.1, P_hp=0, F_lbf=0.1, D=0)
+    #bad_train3 = Train(m_lb=0.1, P_hp=0.1, F_lbf=0, D=0)
+    #bad_train4 = Train(m_lb=0.1, P_hp=0.1, F_lbf=0.1, D=-0.1)
+    #train.calc_accel_time(-0.1)
+    #train.calc_accel_vel(-0.1)
+    #train.calc_accel_dist(0)
+    #train.calc_brake_time(-0.1)
+    #train.calc_brake_dist(-0.1)
+    #train.stop_to_stop_time(d_tot_mi=0, v_max_mph=0.1, t_dwell=0)
+    #train.stop_to_stop_time(d_tot_mi=0.1, v_max_mph=0, t_dwell=0)
+    #train.stop_to_stop_time(d_tot_mi=0.1, v_max_mph=0.1, t_dwell=-0.1)
